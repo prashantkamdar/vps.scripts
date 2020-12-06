af52bf22d084153897c61a35b3caac5509b2adca
prashant <prashantkamdar@gmail.com>
2020-12-06 21:16:37 +0530

    added command line arguments

--- vps.connect.sh
+++ vps.connect.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 #easy conenct to vps server
 
+option=$1
+
+re='^[0-9]+$'
+if [[ $option =~ $re ]] ; then
+   if [ $option -eq 1 ]
+   then
+      echo "Now connecting to VirMach 107.173.51.44 - 512MB/15GB/1TB"
+      ssh root@107.173.51.44
+      exit 1
+   elif [ $option -eq 2 ]
+   then
+      echo "Now connecting to VirMach 172.245.228.110 - 384MB/5GB/500GB"
+      ssh root@172.245.228.110
+      exit 1
+   elif [ $option -eq 3 ]
+   then
+      echo "Now connecting to VirMach 23.95.234.100 - 384MB/5GB/100GB"
+      ssh root@23.95.234.100
+      exit 1
+   elif [ $option -eq 4 ]
+   then
+      echo "Now connecting to RackNerd 107.174.240.162 - 1.5GB/20GB/6TB"
+      ssh root@107.174.240.162
+      exit
+   fi
+fi
+
 clear
 
 echo -e "1. VirMach 107.173.51.44    - 512MB/15GB/1TB"
