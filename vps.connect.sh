74881374d608987936ff5652e2bd5ac106910f48
prashant <prashantkamdar@gmail.com>
2020-12-06 19:59:30 +0530

    second

--- vps.connect.sh
+++ vps.connect.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 #easy conenct to vps server
-
+clear
 echo -e "Which server do you want to connect to?"
+echo -e "1. VirMach 107.173.51.44    - 512MB/15GB/1TB"
+echo -e "2. VirMach 172.245.228.110  - 384MB/5GB/500GB"
+echo -e "3. VirMach 23.95.234.100    - 384MB/5GB/100GB"
+echo -e "4. RackNerd 107.174.240.162 - 1.5GB/20GB/6TB"
+
